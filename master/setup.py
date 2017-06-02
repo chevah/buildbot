47b41628810b01a5d79b1f8b235089e5713de141
Adi Roiban <adi.roiban@chevah.com>
2017-06-02 22:23:41 +0100

    Update github status from 0.8.12. Release 0.8.11.c8.

--- master/setup.py
+++ master/setup.py
@@ -132,7 +132,7 @@ public_version = version.replace('-', '.')
 
 setup_args = {
     'name': "buildbot",
-    'version': public_version + '.c7',
+    'version': public_version + '.c8',
     'description': "BuildBot build automation system",
     'long_description': long_description,
     'author': "Brian Warner",
